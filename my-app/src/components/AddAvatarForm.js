7a31a432823703a5d3696f77e58929a1af8965e3
calvin <ckodalo@gmail.com>
2022-10-06 15:10:31 +0300

    implemented post

--- my-app/src/components/AddAvatarForm.js
+++ my-app/src/components/AddAvatarForm.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function AddAvatarForm() {
+function AddAvatarForm({handleChange, handleSubmit}) {
 
     return (
         <div className="ui segment">
       <form className="ui form">
-        <div className="inline fields">
-          <input type="text" name="name"/>
-          <input type="text" name="gender" placeholder="gender"/>
-          <input type="url" name="url" placeholder="url"/>
+        <div className="inline fields" >
+          <input type="text" name="name" placeholder="name" onBlur ={handleChange}/>
+          <input type="text" name="gender" placeholder="gender" onBlur = {handleChange}/>
+          <input type="url" name="url" placeholder="url" onBlur = {handleChange}/>
         </div>
-        <button>
+        <button onClick = {handleSubmit}>
           Add Avatar
         </button>
       </form>
--- my-app/src/components/LandingPage.js
+++ my-app/src/components/LandingPage.js
@@ -6,7 +6,8 @@ import AddAvatarForm from "./AddAvatarForm";
 function LandingPage() {
   
     const [avatars, setAvatars] = useState([])
-    
+    const [formData, setFormData] = useState([])
+
     useEffect( () => {
     fetch("http://localhost:3000/Avatars")
         .then(res => res.json())
@@ -14,9 +15,33 @@ function LandingPage() {
 
     }, [])
 
+function handleChange (e) {
+    
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value 
+    })
+
+
+    //setAvatars(avatars.map(avatar => avatar === avatarTarget ? {...avatar, new: true} : avatar))     
+
+}    
+
+function handleSubmit () {
+        fetch("http://localhost:3000/Avatars", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+
     return (
     <div>
-         <AddAvatarForm/>
+         <AddAvatarForm handleChange = {handleChange} handleSubmit = {handleSubmit}/>
         <AvatarCollection avatars = {avatars}/>
        </div>
   );
