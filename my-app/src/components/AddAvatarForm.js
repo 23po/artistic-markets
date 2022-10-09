05db97645a3ac26b960ca01d107508d459d413ed
calvin <ckodalo@gmail.com>
2022-10-09 15:31:26 +0300

    messing with ui

--- my-app/src/components/AddAvatarForm.js
+++ my-app/src/components/AddAvatarForm.js
@@ -1,4 +1,70 @@
-import React from "react";
+import { Button, Form, Input, Radio, Upload } from 'antd';
+import React, { useState } from 'react';
+import './AddAvatarForm.css'
+import { UploadOutlined } from '@ant-design/icons';
+
+
+const { TextArea } = Input;
+
+
+const AddAvatarForm = () => {
+  const [form] = Form.useForm();
+  const [formLayout, setFormLayout] = useState('vertical');
+
+  const onFormLayoutChange = ({ layout }) => {
+    setFormLayout(layout);
+  };
+
+  const formItemLayout =
+    formLayout === 'vertical'
+      ? {
+          labelCol: {
+            span: 4,
+          },
+          wrapperCol: {
+            span: 8,
+          },
+        }
+      : null;
+  const buttonItemLayout =
+    formLayout === 'vertical'
+      ? {
+          wrapperCol: {
+            span: 10,
+            offset: 4,
+          },
+        }
+      : null;
+  return (
+    <Form
+      //className='input-container'
+      {...formItemLayout}
+      layout={formLayout}
+      form={form}
+      initialValues={{
+        layout: formLayout,
+      }}
+      onValuesChange={onFormLayoutChange}
+    >
+
+      <Upload>
+       <Button icon={<UploadOutlined />}>Click to Upload</Button>
+      </Upload>
+      <Form.Item label="Field A">
+        <Input placeholder="input placeholder" />
+      </Form.Item>
+      <Form.Item label="TextArea">
+          <TextArea rows={4} />
+        </Form.Item>
+      <Form.Item {...buttonItemLayout}>
+        <Button type="primary">Submit</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default AddAvatarForm;
+/*import React from "react";
 
 function AddAvatarForm({handleChange, handleSubmit}) {
   
@@ -21,3 +87,4 @@ function AddAvatarForm({handleChange, handleSubmit}) {
     );
 }
 export default AddAvatarForm
+*/
